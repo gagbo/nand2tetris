cca82987c1afae9bac132acda29f60a7e2979dab
Gerry Agbobada <gagbobada+git@gmail.com>
2017-08-25 23:08:18 +0200

    Removed unusable preprocessor macros

--- projects/06/HackAssembler.h
+++ projects/06/HackAssembler.h
@@ -6,21 +6,6 @@
 #include <stdio.h>
 #include <string.h>
 
-#define JGT 1
-#define JEQ 2
-#define JGE 3
-#define JLT 4
-#define JNE 5
-#define JLE 6
-#define JMP 7
-#define M 1
-#define D 2
-#define MD 3
-#define A 4
-#define AM 5
-#define AD 6
-#define AMD 7
-
 typedef struct Instructions Instruction;
 
 Instruction convertStringToInstruction(char* AssemblyLine);
