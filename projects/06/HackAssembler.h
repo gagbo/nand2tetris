be750609d432faf0c5d98c6d0204064c49c7585d
Gerry Agbobada <gagbobada+git@gmail.com>
2017-08-25 23:07:31 +0200

    Added header guards

--- projects/06/HackAssembler.h
+++ projects/06/HackAssembler.h
@@ -1,3 +1,6 @@
+#ifndef HACKASSEMBLER_H_
+#define HACKASSEMBLER_H_
+
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
@@ -22,5 +25,6 @@ typedef struct Instructions Instruction;
 
 Instruction convertStringToInstruction(char *AssemblyLine);
 // Converts num to it's 16 bit binary representation, and puts it in str
-void getBin(int16_t num, char *str);
-void tobinstr(int16_t value, int bitsCount, char *output);
+void getBin(int16_t num, char* str);
+void tobinstr(int16_t value, int bitsCount, char* output);
+#endif  // HACKASSEMBLER_H_
