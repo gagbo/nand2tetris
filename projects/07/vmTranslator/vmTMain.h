f71aa7ea363a6663ff69a6623e7154cefb7ca9a1
Gerry Agbobada <gagbobada+git@gmail.com>
2017-08-29 22:36:54 +0200

    file structure

---
+++ projects/07/vmTranslator/vmTMain.h
@@ -0,0 +1,13 @@
+#ifndef _VMTMAIN_H_
+#define _VMTMAIN_H_
+#ifndef __STDC_WANT_LIB_EXT2__
+#define __STDC_WANT_LIB_EXT2__ 1
+#endif  // __STDC_WANT_LIB_EXT2__
+
+#include <stdio.h>
+#include <string.h>
+
+#include "vmTParser.h"
+#include "vmTWriter.h"
+
+#endif  // _VMTMAIN_H_
