ddcccc5923b4cddbfe37a1f0b13d3099f2fc8982
Gerry Agbobada <gagbobada+git@gmail.com>
2017-09-02 15:56:22 +0200

    Added structure to test modules

--- projects/07/vmTranslator/vmTMain.c
+++ projects/07/vmTranslator/vmTMain.c
@@ -1,3 +1,45 @@
 #include "vmTMain.h"
 
-int main(int argc, char** argv) { return 0; }
+int main(int argc, char **argv) {
+    char filename[50];
+    if (argc > 2) {
+        printf("Too many arguments supplied.\n");
+        return 1;
+    } else if (argc <= 1) {
+        printf("One argument expected.\n");
+        return 1;
+    } else {
+        strcpy(filename, argv[1]);
+    }
+
+    // Allocate filestream and parse it
+    FILE *filestream = NULL;
+    LabelCounter *p_LabelCounter = NULL;
+    filestream = fopen(filename, "r");
+    if (filestream == NULL) {
+        fprintf(stderr, "Could not open %s\n", filename);
+        return 1;
+    }
+
+    char line[LINE_BUFFERSIZE];
+    char **command = calloc(MAX_COMMAND_WORDS, sizeof(char *));
+    while (fgets(line, LINE_BUFFERSIZE - 1, filestream) != NULL) {
+        int command_length = parse_line(line, command);
+        fprintf(stderr, "%d : ", command_length);
+        if (command_length == 0) {
+            fprintf(stderr, "%s\n", line);
+            continue;
+        } else {
+            // This is where we should call the writing functions
+            for (int i = 0; i < command_length; ++i) {
+                fprintf(stderr, "%s-", command[i]);
+            }
+            fprintf(stderr, "\n");
+        }
+    }
+
+    free(command);
+    fclose(filestream);
+
+    return 0;
+}
