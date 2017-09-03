cdf762929ebaa0e1670a531300188f51f85e51c2
Gerry Agbobada <gagbobada+git@gmail.com>
2017-09-03 21:17:06 +0200

    Write to .asm output file

--- projects/07/vmTranslator/vmTMain.c
+++ projects/07/vmTranslator/vmTMain.c
@@ -2,7 +2,9 @@
 
 int main(int argc, char **argv) {
     char filename[50];
+    char output_filename[50];
     char *filename_copy = NULL;
+    char *filename_copy2 = NULL;
     char *file_label = NULL;
     if (argc > 2) {
         printf("Too many arguments supplied.\n");
@@ -13,18 +15,29 @@ int main(int argc, char **argv) {
     } else {
         strcpy(filename, argv[1]);
         filename_copy = strdup(filename);
+        filename_copy2 = strdup(filename);
         char *file_basename = basename(filename_copy);
         file_label = strtok(file_basename, ".");
+        // strtok modified definitely file_basename
+        // so we can just use file_label or file_basename indifferently
+        char *file_dirname = dirname(filename_copy2);
+        strcpy(output_filename, file_dirname);
+        strcat(output_filename, "/");
+        strcat(output_filename, file_label);
+        strcat(output_filename, ".asm");
     }
 
     // Allocate filestream and parse it
     FILE *filestream = NULL;
+    FILE *outputstream = NULL;
     LabelCounter LabelCounter;
     LabelCounter.nb_all = 0;
     filestream = fopen(filename, "r");
+    outputstream = fopen(output_filename, "w");
     if (filestream == NULL) {
         fprintf(stderr, "Could not open %s\n", filename);
         free(filename_copy);
+        free(filename_copy2);
         return 1;
     }
 
@@ -38,8 +51,8 @@ int main(int argc, char **argv) {
             // This is where we should call the writing functions
             const char *asm_dict_file =
                 choose_asm_dict_file(command, command_length);
-            write_to_file(stdout, command, &LabelCounter, asm_dict_file, 0,
-                          file_label);
+            write_to_file(outputstream, command, &LabelCounter, asm_dict_file,
+                          0, file_label);
         }
         // Free the memory allocated by parse_line
         // TODO : have alloc and free at the same place
@@ -50,7 +63,9 @@ int main(int argc, char **argv) {
 
     free(command);
     fclose(filestream);
+    fclose(outputstream);
     free(filename_copy);
+    free(filename_copy2);
 
     return 0;
 }
