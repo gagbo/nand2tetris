69b40346e9ec7780af8cc5a070445d37bbc88422
Gerry Agbobada <gagbobada+git@gmail.com>
2017-09-21 15:57:44 +0200

    Argument parsing using Boost::filesystem

--- projects/10/jack_syntax_analyser/main_app/main.cpp
+++ projects/10/jack_syntax_analyser/main_app/main.cpp
@@ -1,24 +1,58 @@
+#include <boost/filesystem.hpp>  // Is included in std=c++17
 #include <iostream>
+#include <string>
+#include <vector>
+
 #include "tokeniser/tokeniser.h"
 
+void parse_arguments(int argc, char** argv,
+                     std::vector<std::string>& files_list);
+
 int main(int argc, char** argv) {
+    std::vector<std::string> files_list;
+    parse_arguments(argc, argv, files_list);
+
+    for (size_t i = 0; i < files_list.size(); ++i) {
+        JackTokeniser token_machine(files_list[i].c_str());
+        while (token_machine.hasMoreTokens()) {
+            std::cout << "Token found : " << token_machine.getToken() << " - "
+                      << static_cast<int>(token_machine.getTokenType()) << "\n";
+            token_machine.showState();
+            token_machine.advance();
+        }
+
+        std::cerr << "The rest is to be implemented by us \n";
+    }
+
+    return 0;
+}
+
+void parse_arguments(int argc, char** argv,
+                     std::vector<std::string>& files_list) {
+    // standard C++17 will use Boost::filesystem
+    std::string argument;
     if (argc != 2) {
         std::cerr << "Print usage and exit because wrong number of arguments\n";
-        return 1;
+        exit(1);
+        return;
     } else {
-        std::string argument = argv[1];
+        argument = argv[1];
         std::cout << "Parsed argument : " << argument << "\n";
     }
 
-    JackTokeniser token_machine(argv[1]);
-    while (token_machine.hasMoreTokens()) {
-        std::cout << "Token found : " << token_machine.getToken() << " - "
-                  << static_cast<int>(token_machine.getTokenType()) << "\n";
-        token_machine.showState();
-        token_machine.advance();
+    boost::filesystem::path p(argument);
+
+    if (boost::filesystem::exists(p)) {
+        if (boost::filesystem::is_regular_file(p)) {
+            std::cerr << "Unique file mode\n";
+            files_list.push_back(argument);
+        } else if (boost::filesystem::is_directory(p)) {
+            std::cerr << "Directory input detected but not implemented\n";
+        }
+    } else {
+        std::cerr << p << " does not exist\n";
+        exit(1);
     }
 
-    std::cerr << "The rest is to be implemented by us \n";
-
-    return 0;
+    return;
 }
