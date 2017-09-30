5d811ad1ef1b62f451a165c09ff359fddb3fc718
Gerry Agbobada <gagbobada+git@gmail.com>
2017-09-30 16:38:24 +0200

    Beginning of the structure

--- projects/10/jack_syntax_analyser/symbol_table/symbol_table.h
+++ projects/10/jack_syntax_analyser/symbol_table/symbol_table.h
@@ -1,3 +1,16 @@
 #ifndef _SYMBOLTABLE_SYMBOLTABLE_H
 #define _SYMBOLTABLE_SYMBOLTABLE_H
+
+#include <map>
+#include <string>
+#include <tuple>
+
+enum struct JackVariableKind : int { FIELD, STATIC, ARGUMENT, LOCAL };
+
+static const char* KindStrings[] = {"field", "static", "argument", "local"};
+
+inline const char* getTextForKind(int enumVal) { return KindStrings[enumVal]; }
+
+typedef std::tuple<std::string, JackVariableKind, int> SymbolEntry;
+
 #endif /* ifndef _SYMBOLTABLE_SYMBOLTABLE_H */
