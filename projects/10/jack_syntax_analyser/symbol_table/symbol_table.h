dcb7b8c9d1450d0786aba8074bb082a8493db674
Gerry Agbobada <gagbobada+git@gmail.com>
2017-10-01 13:47:46 +0200

    Added symbol table and comment

--- projects/10/jack_syntax_analyser/symbol_table/symbol_table.h
+++ projects/10/jack_syntax_analyser/symbol_table/symbol_table.h
@@ -11,6 +11,15 @@ static const char* KindStrings[] = {"field", "static", "argument", "local"};
 
 inline const char* getTextForKind(int enumVal) { return KindStrings[enumVal]; }
 
+// Tuple of symbol entry. (
+// type - int, Class, bool...- ,
+// kind -local, field // static...- ,
+// count - 0++ -
+// )
+// The tuples will be added in a map indexed by the identifier. This is
+// the implementation of the symbol table.
 typedef std::tuple<std::string, JackVariableKind, int> SymbolEntry;
+
+typedef std::map<std::string, SymbolEntry> JackSymbolTable;
 
 #endif /* ifndef _SYMBOLTABLE_SYMBOLTABLE_H */
